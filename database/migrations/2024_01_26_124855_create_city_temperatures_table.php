01ca1c5890de78d2248d8cafd83804dc50945094
Gabor Reszler <hello@gaborreszler.dev>
2024-02-01 14:07:40 +0100

    Add migration files

---
+++ database/migrations/2024_01_26_124855_create_city_temperatures_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('city_temperatures', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('city_id')->constrained(
+                table: 'cities', indexName: 'city_temperatures_city_id'
+            )->cascadeOnDelete();
+            $table->float('temperature');
+            $table->unsignedTinyInteger('relative_humidity');
+            $table->float('wind_speed');
+            $table->json('current');
+            $table->json('current_units');
+            $table->json('hourly');
+            $table->json('hourly_units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('city_temperatures');
+    }
+};
